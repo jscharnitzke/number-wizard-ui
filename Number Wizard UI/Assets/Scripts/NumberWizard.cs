b9b2be988e2457bb7141a3e18a36dad2efdf6410
Joe Scharnitzke <j.scharnitzke@gmail.com>
2019-10-30 09:21:11 -0700

    import controller and refactor

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+
+    private int minNumber;
+    private int maxNumber;
+    private int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialize();
+    }
+
+    // Update is called once per frame
+
+    void initialize()
+    {
+        minNumber = min;
+        maxNumber = max;
+        guess = average(minNumber, maxNumber);
+        guessText();
+    }
+
+    public void guessHigher()
+    {
+        minNumber = guess;
+        nextGuess();
+    }
+
+    public void guessLower()
+    {
+        maxNumber = guess;
+        nextGuess();
+    }
+
+    void nextGuess()
+    {
+        guess = average(minNumber, maxNumber);
+    }
+
+    int average(int a, int b)
+    {
+        return (a + b) / 2;
+    }
+
+    void gameOver()
+    {
+        initialize();
+    }
+}
